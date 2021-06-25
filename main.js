4e81b5e69f1fd860e1d86ba00d403e5e01c75740
raznan <muhammadraznan@satpaminternet.com>
2021-06-25 09:15:31 +0700

    base

---
+++ main.js
@@ -0,0 +1,405 @@
+class InfoTable extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({mode: 'open'});
+    const table = document.createElement('table');
+    const colgroup = document.createElement('colgroup');
+
+    const style = document.createElement('style');   
+    const labelColor = '#519ABA';
+    const seperatorColor = '#436B24';
+    const textColor = '#E5E5E5';
+
+    const nameRow = this._createInfoRow("NAME", "MUHAMMAD RAZNAN");
+    const emailRow = this._createInfoRow("EMAIL", "***********@**********.com");
+    const githubRow = document.createElement('a').appendChild(this._createInfoRow('GITHUB', "https://github.com/scr1pti3"));
+
+    style.textContent = `
+      table {
+        border-collapse: collapse;
+        width: 100%;
+      }
+
+      .label { 
+        width: 100px;
+        color: ${labelColor}
+      }
+      .seperator {
+        width: 30px;
+        color: ${seperatorColor}
+      }
+      .text { color: ${textColor} }
+    `;
+
+    table.append(colgroup, nameRow, emailRow, githubRow);
+    
+    shadow.append(style, table)
+  }
+
+  _createInfoRow(label, value) {
+    let trElement = document.createElement('tr');
+
+    let labelElement = document.createElement('td');
+    let seperatorElement = document.createElement('td');
+    let textElement = document.createElement('td');
+
+    labelElement.setAttribute('class', 'label');
+    seperatorElement.setAttribute('class', 'seperator');
+    textElement.setAttribute('class', 'text');
+
+    labelElement.innerHTML = label;
+    seperatorElement.innerHTML = "|";
+    textElement.innerHTML = value;
+
+    trElement.append(labelElement, seperatorElement, textElement);
+
+    return trElement;
+  }
+}
+
+class CarouselTimeline extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({mode: 'open'});
+    
+    const style = document.createElement('style');   
+    const fontColor = '#6D8086';
+
+    this.currentPage = 1;
+    this.totalPage = 0;
+    this._carouselItemGap = 50;
+    this._carouselItemSizes = {
+      mobile: this.parentNode.clientWidth,           //in pixels
+      desktop: (this.parentNode.clientWidth - 50)/2, //in pixels
+    };
+    this._carouselContainer = document.createElement('div');
+    this._carouselItemContainer = document.createElement('div');
+    this._pagination = document.createElement('div');
+    this.carouselItems = [];
+    this._pagination.setAttribute('class', 'pagination');
+
+    let imgWidth = this._carouselItemSizes.desktop;
+    
+
+
+    for (let x = 0; x < 5; ++x)
+      this.addItem(`https://via.placeholder.com/${imgWidth}x300`, "Lorem Ipsum", 
+        `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
+      );
+
+    style.textContent = `
+      * {
+        padding: 0px;
+        margin: 0px;
+        box-sizing: border-box;
+      } 
+
+      .no-select {
+        -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Safari */
+        -khtml-user-select: none; /* Konqueror HTML */
+        -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+        user-select: none;
+      }
+
+      .carousel-container {
+        margin: inherit;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+      }
+
+      .item-container {
+        position: relative; 
+        display: flex;
+        gap: ${this._carouselItemGap}px;
+      }
+
+      .carousel-item {
+        transition: all 1s;
+        min-width: 100%; 
+      }
+
+      .headline {
+        margin-top: 1rem;
+        display: flex;
+        align-items: center;
+      }
+
+      .title {
+        margin-right: 1rem;
+      }
+
+      .link, .title {
+        font-weight: 700 !important;
+        text-transform: uppercase;
+        word-spacing: -0.4rem;
+      }
+
+      .link, .title, .description {
+        color: ${fontColor}
+      }
+
+      .hline {
+        border: 2px solid ${fontColor};
+        height: 0px;
+        flex-grow: 1;
+      }
+
+      .description {
+        margin: 1rem 0rem;
+        text-align: justify;
+      }
+
+      .footer {
+        display: flex;
+        align-items: center;
+      }
+
+      .link {
+        margin-left: 1rem;
+        text-decoration: none;
+        transition: color 600ms ease-out;
+      }
+
+      .link:hover {
+        color: #CBCB41;
+      }
+
+      .pagination {
+        margin-top: 77px;
+        display: flex;
+        gap: 20px; 
+        align-items: center;
+        justify-content: center;
+      }
+
+      .page {
+        background-color: #6D8086;
+        width: 50px;
+        height: 20px;
+        border-width: 0px;
+        cursor: pointer;
+      }
+
+      .page-active {
+        background-color: #CBCB41;
+      }
+
+      .sliding {
+        transition: left .2s ease-out;
+      }
+
+      @media only screen and (min-width: ${minDesktopWidth}px) {
+        .carousel-item {
+          min-width: calc((100% - ${this._carouselItemGap}px) / 2 );
+        }
+      }
+    `;
+
+    this._carouselItemContainer.setAttribute('class', 'item-container no-select');
+    this._carouselItemContainer.style.left = "0px";
+    this._carouselContainer.appendChild(this._carouselItemContainer);
+    this._carouselContainer.setAttribute('class', 'carousel-container no-select');
+
+    shadow.append(style, this._carouselContainer, this._pagination);
+  } 
+
+  connectedCallback() {
+    let itemContainer = this._carouselItemContainer;
+    let offsetInitial = itemContainer.offsetLeft; 
+    let threshold = this.parentNode.clientWidth * 0.17;
+    let mouseV1 = 0;
+    let mouseV2 = 0;
+    
+    var dragStart = (ed) => {
+      ed.preventDefault();
+      if (!this.isSliding()) {
+        this._slideInitial = itemContainer.offsetLeft;
+        this._carouselContainer.onmousemove = dragMove;
+        this._carouselContainer.onmouseup = dragEnd; 
+        this._carouselContainer.onmouseleave = dragEnd;
+
+        mouseV1 = ed.clientX;
+        mouseV2 = 0;
+      }
+    }
+
+    var dragMove = (em) => {
+      mouseV2 = mouseV1 - em.clientX;
+      mouseV1 = em.clientX;
+      itemContainer.style.left = `${itemContainer.offsetLeft - mouseV2}px`;
+    }
+
+    var dragEnd = (e) => {
+      this._slideTerminal = itemContainer.offsetLeft; 
+      let distance = this._slideTerminal - this._slideInitial;
+      
+      let dir = Math.sign(distance);
+      let targetPage = Math.abs(distance) > threshold ? clamp(this.currentPage + dir * -1, 1, this.totalPage) : this.currentPage;
+
+      this._carouselContainer.onmousemove = null;
+      this._carouselContainer.onmouseleave = null;
+
+      if(dir)
+        if(targetPage == this.currentPage) 
+          this._slide(this._slideInitial);
+        else 
+          this.changePage(targetPage, distance);
+      
+    }
+ 
+    this._slideInitial = 0;
+    this._slideTerminal = 0;
+    this._carouselItemSize = this._carouselItemContainer.firstElementChild.clientWidth;
+
+    this._pagination.firstElementChild.classList.add('page-active');
+    this._carouselContainer.onmousedown = dragStart;
+    this._carouselContainer.ontouchstart = dragStart;
+    this._carouselContainer.ontouchend = dragEnd;
+
+    itemContainer.addEventListener('transitionend', () => {
+        itemContainer.classList.remove("sliding");
+        this._slideInitial = itemContainer.offsetLeft;
+      }
+    );
+    addResizeEvent(this._carouselContainer, () => {
+      console.log(this);
+    });
+  }
+
+  _slide(pos) {
+    this._carouselItemContainer.style.left = `${pos}px`;
+    this._carouselItemContainer.classList.add('sliding');
+  }
+
+  addItem(imgSrc, titleLabel, descContent, linkHref) {
+    const itemContainer = document.createElement('div');
+    const figure = document.createElement('figure');
+    const figureImg = document.createElement('img');
+
+    const headline = document.createElement('div');
+    const title = document.createElement('h3');
+    const hline = document.createElement('span');
+
+    const description = document.createElement('p');
+
+    const footer = document.createElement('div');
+    const link = document.createElement('a');
+
+    figureImg.setAttribute('srcset', imgSrc);
+    figureImg.onmousedown = e => e.preventDefault();
+    figure.appendChild(figureImg);
+
+    headline.setAttribute('class', 'headline')
+    headline.append(title, hline)
+    title.setAttribute('class', 'title');
+    title.textContent = titleLabel;
+    hline.setAttribute('class', 'hline');
+
+    description.textContent = descContent;
+    description.setAttribute('class', 'description')
+
+    footer.setAttribute('class', 'footer');
+    const fline = hline.cloneNode(true);
+    link.setAttribute('href', linkHref);
+    link.setAttribute('class', 'link');
+    link.textContent = "read more";
+    footer.append(fline, link);
+
+    itemContainer.setAttribute('class', 'carousel-item');
+    itemContainer.append(figure, headline, description, footer);
+
+
+    if(isMediaDesktop()) {
+      if(this.carouselItems.length % 2 == 0)
+        this.addPage();
+    }
+    else {
+      this.addPage();
+    }
+    
+    this._carouselItemContainer.appendChild(itemContainer);
+    this.carouselItems.push(itemContainer);
+  }
+
+  addPage() {
+    this.totalPage += 1;
+
+    let page = document.createElement('button');
+    page.setAttribute('class', 'page');
+    page.setAttribute('data-index', this._pagination.childElementCount + 1);
+    page.onclick = (e) => {
+      let targetPage = parseInt(e.currentTarget.getAttribute('data-index'));
+      if(this.currentPage != targetPage)
+        this.changePage(targetPage);
+    };
+    this._pagination.appendChild(page);
+  }
+
+  changePage(to, slideDelta = 0) {
+    let from = this.currentPage;
+    let pageWidth = this._carouselContainer.clientWidth;
+    let distance = to - from;
+    let totalGaps = this._carouselItemGap * distance;
+    let offset = (distance * pageWidth + totalGaps + slideDelta) * -1;
+    let fromPos = this._carouselItemContainer.offsetLeft; 
+    let toPos = fromPos + offset; 
+    
+    this._slide(toPos);
+    this._pagination.children[from - 1].classList.remove('page-active')
+    this._pagination.children[to - 1].classList.add('page-active');
+    this.currentPage = to;
+  }
+
+  isSliding() {
+    return this._carouselItemContainer.classList.contains('sliding');
+  }
+}
+
+var minDesktopWidth = 768; 
+var addResizeEvent = (function() {
+  const callbackCollection = [];
+
+  if(window.ResizeObserver) {
+    return (function(element, callback) {
+      const resizeObserver = new ResizeObserver(callback);
+      resizeObserver.observe(element);
+      return resizeObserver;
+    });
+  }
+
+  // Polyfill for ResizeObserver
+  return (function(element, callback) {
+    callbackCollection.push(callback);
+    window.onresize = () => {
+      for(const c of callbackCollection)
+        c.call(element);
+    }
+    return null;
+  });
+})();
+
+customElements.define('info-table', InfoTable);
+customElements.define('carousel-timeline', CarouselTimeline);
+
+function clamp(n, min, max) {
+  return Math.min(Math.max(n, min), max);
+}
+
+function isMediaDesktop() {
+  return getViewportWidth() >= minDesktopWidth;
+}
+
+function getViewportWidth() {
+  return document.documentElement.scrollWidth;
+}
+
+function getViewportHeight() {
+  return document.documentElement.scrollHeight;
+}
